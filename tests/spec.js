ce98e26653306cc5d357a0d549ea88e60ade023c
William Herrera <william@gravitoncloud.com>
2024-04-12 20:50:32 -0700

    updated test spec

---
+++ tests/spec.js
@@ -0,0 +1,6 @@
+// Description: This file contains the unit tests for the database functions.
+console.log("Unit Tests");
+
+console.log("TODO: Implement tests for the database functions.");
+
+process.exit(0); // Exit with success code
